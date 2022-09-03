556a53bf4cd3b347d30a6f27594906e49dea6cb6
Ana Packer <anaamaria.packer@gmail.com>
2022-09-03 19:24:35 -0300

    quizzes carregados e renderizados na tela

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-/* Teste de funcionamento 
-alert('Olá);*/
+function carregarQuizzes() {
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    promessa.then(renderizarQuizzes);
+
+}
+
+
+function renderizarQuizzes(resposta) {
+    console.log(resposta.data);
+
+    const todosQuizzes = resposta.data;
+
+    const ul = document.querySelector(".box-quizzes");
+
+    for (let i = 0; i < todosQuizzes.length; i++) {
+        ul.innerHTML += `
+                    <li class="caixa-quizz">
+                        <div class="gradient"></div>
+                        <img src="${todosQuizzes[i].image}" />
+                        <span>${todosQuizzes[i].title}</span>
+                    </li>
+        `;
+
+
+    }
+
+
+
+}
+
+carregarQuizzes();
